fac6629ba2463cd4833d0bbebd0a0a23fadfca0a
dhyo.101 <83352063+dhyo-detallboy@users.noreply.github.com>
2022-03-17 23:33:14 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello-Project--Alif Dio")
+#Hello Project -- 1
+
+#inputan
+kata1 = input("masukan kata pertama : ")
+kata2 = input("masukan kata kedua : ")
+
+print("")
+
+#output
+print(kata1, kata2)
